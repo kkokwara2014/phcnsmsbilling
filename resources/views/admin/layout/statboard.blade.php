1ca42985f0a067fefe7d03e6fcc49e355663379d
kkokwara2014 <kkokwara2014@gmail.com>
2020-03-16 06:18:51 +0100

    Update statboard.blade.php

--- resources/views/admin/layout/statboard.blade.php
+++ resources/views/admin/layout/statboard.blade.php
@@ -4,12 +4,12 @@
   <div class="row">
     @if (Auth::user()->role->id==1||Auth::user()->role->id==2)
     <div class="row">
-      <div class="col-lg-2 col-xs-6">
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-aqua">
           <div class="inner">
           <h3>0</h3>
-  
+
             <p>Allocated Projects</p>
           </div>
           <div class="icon">
@@ -17,13 +17,13 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
-      <div class="col-lg-2 col-xs-6">
+      </div> --}}
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-fuchsia">
           <div class="inner">
           <h3>9</h3>
-  
+
             <p>Unallocated Projects</p>
           </div>
           <div class="icon">
@@ -31,14 +31,14 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
+      </div> --}}
       <!-- ./col -->
-      <div class="col-lg-2 col-xs-6">
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-green">
           <div class="inner">
             <h3>2</h3>
-  
+
             <p>Students</p>
           </div>
           <div class="icon">
@@ -46,14 +46,14 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
+      </div> --}}
       <!-- ./col -->
-      <div class="col-lg-2 col-xs-6">
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-yellow">
           <div class="inner">
             <h3>2</h3>
-  
+
             <p>Lecturers</p>
           </div>
           <div class="icon">
@@ -61,13 +61,13 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
-      <div class="col-lg-2 col-xs-6">
+      </div> --}}
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-light-blue-active">
           <div class="inner">
             <h3>6</h3>
-  
+
             <p>Project Coordinator</p>
           </div>
           <div class="icon">
@@ -75,14 +75,14 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
+      </div> --}}
       <!-- ./col -->
-      <div class="col-lg-2 col-xs-6">
+      {{-- <div class="col-lg-2 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-maroon-active">
           <div class="inner">
             <h3>1</h3>
-  
+
             <p>Admins</p>
           </div>
           <div class="icon">
@@ -90,7 +90,7 @@
           </div>
           <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
-      </div>
+      </div> --}}
       <!-- ./col -->
     </div>
 
